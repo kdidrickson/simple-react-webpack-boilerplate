dce869f4b3ed4b91958ab2d31cc04e1a7a1d6d9c
Kristof Didrickson <kristof.didrickson@lionassociates.com>
2017-07-08 15:53:20 -0700

    don't display well without label and value

--- scripts/components/Well.js
+++ scripts/components/Well.js
@@ -2,27 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Well = ({
-	label,
-	value
-}) => (
-	<div className="well">
-		<div
-			className="well-value"
-			children={value}
-		/>
-		<div
-			className="well-label"
-			children={label}
-		/>
-	</div>
-);
+  label,
+  value
+}) => label && value ? (
+  <div className="well">
+    <div
+      className="well-value"
+      children={value}
+    />
+    <div
+      className="well-label"
+      children={label}
+    />
+  </div>
+) : null;
 
 Well.propTypes = {
-	label: PropTypes.string.isRequired,
-	value: PropTypes.oneOfType([
-		PropTypes.string,
-		PropTypes.number
-	]).isRequired
+  label: PropTypes.string.isRequired,
+  value: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number
+  ]).isRequired
 }
 
 export default Well;
